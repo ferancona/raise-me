14858778e1d3879ed4bc0ed0507dbbbddd713f33
Fernando Ancona <f.anconac@gmail.com>
2022-09-07 22:40:39 +0100

    feat(model): separate parser logic

--- raise_me/models/deployment.py
+++ raise_me/models/deployment.py
@@ -3,11 +3,8 @@ import pathlib
 from dataclasses import dataclass, field
 from typing import Dict, List, Union
 
-from ruamel.yaml import YAML
-from ruamel.yaml.constructor import DuplicateKeyError
-
-from raise_me import PROVIDERS
-from .event import EventSource, EventTarget, RaiseEvent
+from .event import RaiseEvent
+from raise_me.parser import DeploymentParser
 
 
 @dataclass
@@ -48,59 +45,59 @@ class Deployment:
     ...
 
 
-class DeploymentParser:
-    @classmethod
-    def parse_from_yaml(cls, path: pathlib.Path) -> "Deployment":
-        parser = YAML(typ='safe')
-        try:
-            with open(path, 'r') as f:
-                yaml_ = parser.load(f.read())
-        except DuplicateKeyError as err:
-            # Log. No duplicate keys allowed.
-            raise err
-        return Deployment(events=cls.parse_events(yaml_=yaml_))
+# class DeploymentParser:
+#     @classmethod
+#     def parse_from_yaml(cls, path: pathlib.Path) -> "Deployment":
+#         parser = YAML(typ='safe')
+#         try:
+#             with open(path, 'r') as f:
+#                 yaml_ = parser.load(f.read())
+#         except DuplicateKeyError as err:
+#             # Log. No duplicate keys allowed.
+#             raise err
+#         return Deployment(events=cls.parse_events(yaml_=yaml_))
     
-    @classmethod
-    def parse_events(cls, yaml_: YAML) -> List[RaiseEvent]:
-        if 'Events' not in yaml_:
-            raise SyntaxError('"Events" configuration not found.')
+#     @classmethod
+#     def parse_events(cls, yaml_: YAML) -> List[RaiseEvent]:
+#         if 'Events' not in yaml_:
+#             raise SyntaxError('"Events" configuration not found.')
         
-        if not isinstance(yaml_['Events'], dict):
-            raise SyntaxError('"Events" configuration not found.')
+#         if not isinstance(yaml_['Events'], dict):
+#             raise SyntaxError('"Events" configuration not found.')
 
-        raise_events: List[RaiseEvent] = []
-        for name, event_meta in yaml_['Events'].items():
-            if not cls.valid_source(event_meta['Source']):
-                raise SyntaxError('Source from event "{}" malformed: {}.'
-                    .format(name, event_meta['Source']))
-            for endpoint in event_meta['Targets']:
-                if not cls.valid_target(endpoint):
-                    raise SyntaxError('Target from event "{}" unreachable: {}.'
-                        .format(name, endpoint))
+#         raise_events: List[RaiseEvent] = []
+#         for name, event_meta in yaml_['Events'].items():
+#             if not cls.valid_source(event_meta['Source']):
+#                 raise SyntaxError('Source from event "{}" malformed: {}.'
+#                     .format(name, event_meta['Source']))
+#             for endpoint in event_meta['Targets']:
+#                 if not cls.valid_target(endpoint):
+#                     raise SyntaxError('Target from event "{}" unreachable: {}.'
+#                         .format(name, endpoint))
             
-            src = event_meta['Source'].split('::')
-            raise_events.append(
-                RaiseEvent(
-                    logical_name=name,
-                    source=EventSource(
-                        provider=src[0],
-                        service=src[1],
-                    ),
-                    targets=[EventTarget(endpoint=ep) 
-                             for ep in event_meta['Targets']])
-            )
-        return raise_events
+#             src = event_meta['Source'].split('::')
+#             raise_events.append(
+#                 RaiseEvent(
+#                     logical_name=name,
+#                     source=EventSource(
+#                         provider=src[0],
+#                         service=src[1],
+#                     ),
+#                     targets=[EventTarget(endpoint=ep) 
+#                              for ep in event_meta['Targets']])
+#             )
+#         return raise_events
     
-    @classmethod
-    def valid_source(self, src: str) -> bool:
-        """Validates RaiseEvent.Source format (e.g., AWS::S3)."""
-        split = src.split('::')
-        return len(split) == 2 and \
-            split[0] in PROVIDERS and \
-            split[1] in PROVIDERS[split[0]]
+#     @classmethod
+#     def valid_source(self, src: str) -> bool:
+#         """Validates RaiseEvent.Source format (e.g., AWS::S3)."""
+#         split = src.split('::')
+#         return len(split) == 2 and \
+#             split[0] in PROVIDERS and \
+#             split[1] in PROVIDERS[split[0]]
 
-    @classmethod
-    def valid_target(self, endpoints: List[str]) -> bool:
-        # TODO: Check if endpoints are reachable.
-        ...
-        return True
+#     @classmethod
+#     def valid_target(self, endpoints: List[str]) -> bool:
+#         # TODO: Check if endpoints are reachable.
+#         ...
+#         return True
