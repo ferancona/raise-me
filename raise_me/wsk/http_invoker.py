703280efd10a66fc015fab73c25b23d1cfa271e3
Fernando Ancona <f.anconac@gmail.com>
2022-09-11 20:10:39 +0100

    feat(openwhisk.actions): generic http_invoker

---
+++ raise_me/wsk/http_invoker.py
@@ -0,0 +1,17 @@
+import requests
+from typing import Dict
+
+
+def main(args: dict) -> dict:
+    event: Dict = args.get('event')
+    http_target: Dict = args.get('http_target')
+
+    http_method: str = http_target.get('method')
+    url: str = http_target.get('url')
+    
+    if http_method == 'get':
+        r = requests.get(url)
+    else: # post
+        r = requests.post(url, data={event})
+    
+    return r.json()
