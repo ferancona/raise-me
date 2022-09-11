6e41474dd390f5a4b98a9672877a60ad85b71b0d
Fernando Ancona <f.anconac@gmail.com>
2022-09-11 20:11:42 +0100

    feat(openwhisk.actions): generic http mediator that invokes http invoker action for every http endpoint

---
+++ raise_me/wsk/http_mediator.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from typing import Dict, List
+
+HTTP_TARGETS: List[Dict]
+AUTH: tuple
+
+
+def main(args: dict) -> dict:
+    event: Dict = args.get('event')
+
+    for target in HTTP_TARGETS:
+        apihost = os.getenv('__OW_API_HOST')
+        ns = os.getenv('__OW_NAMESPACE')
+        invoker_name = 'raise-http-invoker'
+        invoker_url = '{}:443/api/v1/namespaces/{}/actions/{}'.format(
+            apihost, ns, invoker_name)
+        
+        # Invoke 'raise-http-invoker' action.
+        _ = requests.post(
+            invoker_url,
+            auth=AUTH,
+            data={
+                'actionName': invoker_name,
+                'namespace': ns,
+                'payload': {
+                    'event': event,
+                    'http_target': target,
+                },
+                'blocking': False,
+                'result': False,
+            }
+        )
+
+    return {}
