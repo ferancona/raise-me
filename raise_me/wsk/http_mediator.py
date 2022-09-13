3d082bd173a10b30ce00eb68dcb23f34f75c1b07
Fernando Ancona <f.anconac@gmail.com>
2022-09-13 17:41:01 +0100

    refactor: move variables out of loop

--- raise_me/wsk/http_mediator.py
+++ raise_me/wsk/http_mediator.py
@@ -2,6 +2,7 @@ import os
 import requests
 from typing import Dict, List
 
+# Variable values added when mediator action deployed.
 HTTP_TARGETS: List[Dict]
 AUTH: tuple
 
@@ -9,13 +10,13 @@ AUTH: tuple
 def main(args: dict) -> dict:
     event: Dict = args.get('event')
 
+    apihost = os.getenv('__OW_API_HOST')
+    ns = os.getenv('__OW_NAMESPACE')
+    invoker_name = 'raise-http-invoker'
+    invoker_url = '{}:443/api/v1/namespaces/{}/actions/{}'.format(
+        apihost, ns, invoker_name)
+    
     for target in HTTP_TARGETS:
-        apihost = os.getenv('__OW_API_HOST')
-        ns = os.getenv('__OW_NAMESPACE')
-        invoker_name = 'raise-http-invoker'
-        invoker_url = '{}:443/api/v1/namespaces/{}/actions/{}'.format(
-            apihost, ns, invoker_name)
-        
         # Invoke 'raise-http-invoker' action.
         _ = requests.post(
             invoker_url,
