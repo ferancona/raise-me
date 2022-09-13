1fd0dd7c66288a3253fcdeb981cdede4ee07f35b
Fernando Ancona <f.anconac@gmail.com>
2022-09-13 15:42:33 +0100

    feat(openwhisk): REST Openwhisk Client

--- raise_me/wsk/wsk_client.py
+++ raise_me/wsk/wsk_client.py
@@ -1,6 +1,8 @@
-from typing import Dict
+from typing import Dict, List
+
 import requests
 from requests.models import Response
+
 
 class WskClient:
     DEFAULT_NS = '_'
@@ -16,17 +18,17 @@ class WskClient:
         self.url = '{}://{}/api/v1/namespaces/{}'.format(
             "https" if https else "http", api_host, self.ns)
 
-    def create_action(self, 
-                      name: str, 
-                      runtime: str, 
-                      code: str, 
+    def create_action(self,
+                      name: str,
+                      runtime: str,
+                      code: str,
                       main: str=None) -> Response:
         main = main if main else 'main'
-        url = f'{self.url}/actions/{name}?overwrite=true'
+        url = f'{self.url}/actions/{name}'
         r = requests.put(
             url,
             auth=self.auth,
-            data={
+            json={
                 'namespace': self.ns,
                 'name': name,
                 'exec': {
@@ -35,21 +37,122 @@ class WskClient:
                     'main': main,
                 },
                 'publish': True,
-            }
+            },
+            params={'overwrite': 'true'},
+        )
+        return r
+
+    def create_trigger(self, name: str) -> Response:
+        url = f'{self.url}/triggers/{name}'
+        r = requests.put(
+            url,
+            auth=self.auth,
+            json={
+                'namespace': self.ns,
+                'name': name,
+                'publish': True,
+            },
+            params={'overwrite': 'true'},
+        )
+        return r
+
+    def create_rule(self,
+                    name: str,
+                    trigger_name: str,
+                    action_name: str) -> Response:
+        url = f'{self.url}/rules/{name}'
+        r = requests.put(
+            url,
+            auth=self.auth,
+            json={
+                'name': name,
+                'trigger': trigger_name,
+                'action': action_name,
+                'status': 'active',
+                'publish': True,
+            },
+            params={'overwrite': 'true'},
+        )
+        return r
+    
+    def delete_action(self, name: str) -> Response:
+        url = f'{self.url}/actions/{name}'
+        r = requests.delete(url)
+        return r
+    
+    def delete_trigger(self, name: str) -> Response:
+        url = f'{self.url}/triggers/{name}'
+        r = requests.delete(url)
+        return r
+    
+    def delete_rule(self, name: str):
+        url = f'{self.url}/rules/{name}'
+        r = requests.delete(url)
+        return r
+
+    def fire_trigger(self, name: str, args: Dict=None) -> Response:
+        args = args if args else dict()
+        url = f'{self.url}/triggers/{name}'
+        r = requests.post(
+            url,
+            auth=self.auth,
+            json=args,
+            params={
+                'blocking': False,
+                'result': False,
+            },
         )
         return r
     
     def invoke_action(self, name: str, args: Dict=None) -> Response:
+        args = args if args else dict()
         url = f'{self.url}/actions/{name}'
         r = requests.post(
             url,
             auth=self.auth,
-            data={
-                'actionName': name,
-                'namespace': self.ns,
-                'payload': args,
+            json=args,
+            params={
                 'blocking': False,
                 'result': False,
-            }
+            },
         )
         return r
+    
+    def list_actions(self, limit=200) -> List[str]:
+        """Returns list of action names (max_size: 200)."""
+        # TODO: test
+        url = f'{self.url}/actions'
+        r = requests.get(
+            url,
+            auth=self.auth,
+            params={
+                'limit': limit,
+            },
+        )
+        return [action['name'] for action in r.json()]
+    
+    def list_triggers(self, limit=200) -> List[str]:
+        """Returns list of trigger names (max_size: 200)."""
+        # TODO: test
+        url = f'{self.url}/triggers'
+        r = requests.get(
+            url,
+            auth=self.auth,
+            params={
+                'limit': 200,
+            },
+        )
+        return [trigger['name'] for trigger in r.json()]
+    
+    def list_rules(self, limit=200) -> List[str]:
+        """Returns list of rule names (max_size: 200)."""
+        # TODO: test
+        url = f'{self.url}/rules'
+        r = requests.get(
+            url,
+            auth=self.auth,
+            params={
+                'limit': 200,
+            },
+        )
+        return [rule['name'] for rule in r.json()]
