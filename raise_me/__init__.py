767bee77cd75ccfbe93cc2d61c351b8e9da12b1a
Fernando Ancona <f.anconac@gmail.com>
2022-09-13 19:44:39 +0100

    refactor(raise-me): change location of raise-events and raise-config

--- raise_me/__init__.py
+++ raise_me/__init__.py
@@ -7,7 +7,8 @@ PROJECT_PATH = pathlib.Path(__file__).resolve().parent.parent
 RESOURCES_PATH = PROJECT_PATH / 'resources'
 
 OPENWHISK_CHART_PATH = RESOURCES_PATH / 'helm' / 'openwhisk'
-DEPLOYMENT_PATH = PROJECT_PATH / 'config' / 'raise-events.yaml'
+RAISE_DEPLOYMENT_PATH = PROJECT_PATH / 'raise-events.yaml'
+RAISE_CONFIG_PATH = PROJECT_PATH / 'raise-config.yaml'
 CUSTOM_TARGETS_PATH = RESOURCES_PATH / 'custom-targets'
 
 # Providers available.
