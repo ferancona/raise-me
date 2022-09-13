a0a82484c4ef7abc54c7563d3a85cf1a635d299e
Fernando Ancona <f.anconac@gmail.com>
2022-09-13 17:23:24 +0100

    feat(models): add http method 'post'

--- raise_me/models/event/__init__.py
+++ raise_me/models/event/__init__.py
@@ -4,14 +4,17 @@ from raise_me.exceptions import UnrecognizedHttpMethod
 
 from .raise_event import RaiseEvent
 from .source import EventSource
-from .target import EventTarget, HttpTarget, CustomTarget
+from .target import EventTarget, HttpTarget, ActionTarget
 
 
 class HttpMethod(Enum):
     GET='get'
+    POST='post'
 
     @classmethod
     def from_str(cls, val: str) -> "HttpMethod":
         if val == 'get': 
             return HttpMethod.GET
+        elif val == 'post': 
+            return HttpMethod.POST
         raise UnrecognizedHttpMethod(val)
