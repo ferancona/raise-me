3f652aaf5bc406fce53bb81240be6bb770314ecb
Fernando Ancona <f.anconac@gmail.com>
2022-09-13 19:48:17 +0100

    feat(openwhisk): add module variables

---
+++ raise_me/wsk/__init__.py
@@ -0,0 +1,8 @@
+from raise_me import PROJECT_PATH
+
+
+WSK_MODULE_PATH = PROJECT_PATH / 'raise_me' / 'wsk'
+HTTP_INVOKER_PATH = WSK_MODULE_PATH / 'http_invoker.py'
+HTTP_MEDIATOR_PATH = WSK_MODULE_PATH / 'http_mediator.py'
+
+HTTP_INVOKER_ACTION_NAME = 'raise-http-invoker' # Must match 'http_mediator.py'
