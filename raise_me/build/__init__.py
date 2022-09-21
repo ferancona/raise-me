6be8c3f46572c0b0400d174cce5b29708cc5747b
Fernando Ancona <f.anconac@gmail.com>
2022-09-21 20:53:14 +0100

    feat(build): add module variables

---
+++ raise_me/build/__init__.py
@@ -0,0 +1,16 @@
+from raise_me import RAISE_CONFIG_PATH, RESOURCES_PATH
+from raise_me.parser import ConfigParser
+
+
+LAMBDA_RESOURCES_PATH = RESOURCES_PATH / 'aws' / 'lambda'
+CLOUDEVENTS_LAYER_PATH = LAMBDA_RESOURCES_PATH / 'layers' \
+    / 'cloudevents-1.6.1.zip'
+LAMBDA_TRIGGER_PATH = LAMBDA_RESOURCES_PATH / 'functions' \
+    / 'openwhisk_trigger.zip'
+
+
+conf = ConfigParser.from_yaml(path=RAISE_CONFIG_PATH)
+EVENT_BUS_NAME = conf['aws']['event-bus-name']
+OW_ENDPOINT = conf['openwhisk']['endpoint']
+OW_USERNAME = conf['auth']['username']
+OW_PASSWORD = conf['auth']['password']
