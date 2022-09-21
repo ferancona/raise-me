e20232b69f09e358a84ba24961eecead8142c126
Fernando Ancona <f.anconac@gmail.com>
2022-09-21 15:38:37 +0100

    feat(parser): add FilterParser class

--- raise_me/parser/event/source.py
+++ raise_me/parser/event/source.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List, Union
 
 from raise_me import PROVIDERS
 from raise_me.models import EventSource
@@ -31,10 +31,33 @@ class EventSourceParser:
             return False
         
         # Validate 'filters' is a non-empty List[str].
-        if 'filters' in source.keys() and (
-                not isinstance(source['filters'], List) or 
-                len(source['filters']) == 0 or
-                not all([isinstance(f, str) for f in source['filters']])):
+        if 'filters' in source.keys():
+            if not FilterParser.valid(filters=source['filters'], 
+                    provider=source['provider']):
+                return False
+        
+        return True
+
+
+class FilterParser:
+
+    @classmethod
+    def valid(cls, filters: List[str], provider: str) -> bool:
+        """Validates filters syntax in terms of accepted provider formats.
+        
+        AWS event patterns are composed by key/value pairs separated by ':'.
+        GCP Eventrac filters are composed by key/value pairs separated by '='.
+        """
+
+        if not isinstance(filters, List) or \
+                len(filters) == 0 or \
+                not all([isinstance(f, str) for f in filters]):
             return False
+        
+        split_char = ':' if provider == 'aws' else '='
+        for f in filters:
+            split = f.split(split_char)
+            if len(split) < 2 or (len(split[0]) == 0 or len(split[1]) == 0):
+                return False
         
         return True
