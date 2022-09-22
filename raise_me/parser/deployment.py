466b6d339c05f707a10aacdf5f9216ed16d06532
Fernando Ancona <f.anconac@gmail.com>
2022-09-22 16:44:04 +0100

    refactor(parser): decouple yaml file reading

--- raise_me/parser/deployment.py
+++ raise_me/parser/deployment.py
@@ -1,29 +1,20 @@
 import pathlib
 from typing import Dict, Union
 
-from ruamel.yaml import YAML
-from ruamel.yaml.constructor import DuplicateKeyError
-
 from raise_me.models import Deployment
 
+from . import YAMLParser
 from .event import RaiseEventParser
 
 
 class DeploymentParser:
     @classmethod
     def from_yaml(cls, path: Union[str, pathlib.Path]) -> Deployment:
-        path = pathlib.Path(path).resolve()
-        parser = YAML(typ='safe')
-        try:
-            with open(path, 'r') as f:
-                yaml_: Dict = parser.load(f.read())
-        except DuplicateKeyError as err:
-            # Log.
-            raise err
+        yaml_: Dict = YAMLParser.parse(path=path)
         
         if 'Events' not in yaml_:
             raise SyntaxError('"Events" configuration not found.')
-        
+
         if not isinstance(yaml_['Events'], dict):
             raise SyntaxError('"Events" configuration not found.')
         
