ab13e8e1da923b83fc395f3768df8e79ada0cc9e
Fernando Ancona <f.anconac@gmail.com>
2022-09-22 18:24:17 +0100

    refactor: change location of variables to /cloud/__init__.py

--- raise_me/build/__init__.py
+++ raise_me/build/__init__.py
@@ -1,19 +1,2 @@
-from .openwhisk_builder import OpenwhiskBuilder
-
-from raise_me import RAISE_CONFIG_PATH, RESOURCES_PATH
-from raise_me.parser import ConfigParser
-
-
-LAMBDA_RESOURCES_PATH = RESOURCES_PATH / 'aws' / 'lambda'
-CLOUDEVENTS_LAYER_PATH = LAMBDA_RESOURCES_PATH / 'layers' \
-    / 'cloudevents-1.6.1.zip'
-LAMBDA_TRIGGER_PATH = LAMBDA_RESOURCES_PATH / 'functions' \
-    / 'openwhisk_trigger.zip'
-WORKFLOW_TRIGGER_PATH = RESOURCES_PATH / 'gcp' / 'workflows' \
-    / 'openwhisk_trigger.yaml'
-
-conf = ConfigParser.from_yaml(path=RAISE_CONFIG_PATH)
-EVENT_BUS_NAME = conf['aws']['event-bus-name']
-OW_ENDPOINT = conf['openwhisk']['endpoint']
-OW_USERNAME = conf['auth']['username']
-OW_PASSWORD = conf['auth']['password']
+from .openwhisk import OpenwhiskBuilder
+from .cloud import CloudOrchestrator, AWSCloud, GCPCloud
