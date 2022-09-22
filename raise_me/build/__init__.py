085dfce02a95bcd22ffb34ae3cf6116eb2c6d2be
Fernando Ancona <f.anconac@gmail.com>
2022-09-22 16:45:20 +0100

    feat(build): add workflow resource path

--- raise_me/build/__init__.py
+++ raise_me/build/__init__.py
@@ -9,7 +9,8 @@ CLOUDEVENTS_LAYER_PATH = LAMBDA_RESOURCES_PATH / 'layers' \
     / 'cloudevents-1.6.1.zip'
 LAMBDA_TRIGGER_PATH = LAMBDA_RESOURCES_PATH / 'functions' \
     / 'openwhisk_trigger.zip'
-
+WORKFLOW_TRIGGER_PATH = RESOURCES_PATH / 'gcp' / 'workflows' \
+    / 'openwhisk_trigger.yaml'
 
 conf = ConfigParser.from_yaml(path=RAISE_CONFIG_PATH)
 EVENT_BUS_NAME = conf['aws']['event-bus-name']
