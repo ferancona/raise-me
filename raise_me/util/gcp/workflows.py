ffcea81d92889330b1b9354c0f608b648955a716
Fernando Ancona <f.anconac@gmail.com>
2022-09-25 19:32:17 +0100

    fix(util): correct openwhisk url

--- raise_me/util/gcp/workflows.py
+++ raise_me/util/gcp/workflows.py
@@ -16,13 +16,17 @@ class GCPWorkflowAdapter:
         self.workflow: Dict = YAMLParser.parse(path=workflow_path)
     
     def to_pulumi_str(self, trigger_name: str) -> str:
+        ow_url: str = '{}/api/v1/namespaces/{}'.format(
+            self.conf['openwhisk']['endpoint'],
+            self.conf['openwhisk']['namespace'],
+        )
         for step in self.workflow['main']['steps']:
             if 'init_openwhisk_vars' in step:
                 for var in step['init_openwhisk_vars']['assign']:
                     if 'TRIGGER_NAME' in var:
                         var['TRIGGER_NAME'] = trigger_name
                     elif 'OW_URL' in var:
-                        var['OW_URL'] = ...
+                        var['OW_URL'] = ow_url
             
             if 'fire_openwhisk_trigger' in step:
                 ow_auth: Dict[str, str] = self.conf['openwhisk']['auth']
