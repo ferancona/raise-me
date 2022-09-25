3aeffeb92deea7e3b9f3ed0efc44f693aaec18b6
Fernando Ancona <f.anconac@gmail.com>
2022-09-25 18:27:47 +0100

    feat(raise-me): add tests path

--- raise_me/__init__.py
+++ raise_me/__init__.py
@@ -5,11 +5,10 @@ import pathlib
 
 PROJECT_PATH = pathlib.Path(__file__).resolve().parent.parent
 RESOURCES_PATH = PROJECT_PATH / 'resources'
+TESTS_PATH = PROJECT_PATH / 'tests'
 
-OPENWHISK_CHART_PATH = RESOURCES_PATH / 'helm' / 'openwhisk'
 RAISE_DEPLOYMENT_PATH = PROJECT_PATH / 'raise-events.yaml'
 RAISE_CONFIG_PATH = PROJECT_PATH / 'raise-config.yaml'
-CUSTOM_TARGETS_PATH = RESOURCES_PATH / 'custom-targets'
 
 # Providers available.
 PROVIDERS = (
