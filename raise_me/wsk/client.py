9cf28324928a00ca0eb39c1838057d645d4b9896
Fernando Ancona <f.anconac@gmail.com>
2022-09-25 19:10:51 +0100

    refactor: merge client and paginator

--- raise_me/wsk/client.py
+++ raise_me/wsk/client.py
@@ -1,9 +1,7 @@
-from typing import Dict, List
+from typing import Dict, Iterator, List
 
 import requests
 from requests.models import Response
-
-from .paginator import WskPaginator
 
 
 class WskClient:
@@ -175,3 +173,31 @@ class WskClient:
 
     def rule_paginator(self) -> "WskPaginator":
         return WskPaginator(client=self, resource='rule')
+
+
+class WskPaginator:
+    def __init__(self, client: WskClient, resource: str) -> None:
+        self.client = client
+
+        if resource == 'action':
+            self.list_method = self.client.list_actions
+        elif resource == 'trigger':
+            self.list_method = self.client.list_triggers
+        elif resource == 'rule':
+            self.list_method = self.client.list_rules
+        else:
+            raise AttributeError(
+                f'No paginator found for resource {resource}.')
+    
+    def paginate(self) -> Iterator[str]:
+        limit = 200
+        skip = 0
+        resources = self.list_method(limit=limit, skip=skip)
+        while len(resources) == 200:
+            for action in resources:
+                yield action
+            skip += 200
+            resources = self.list_method(limit=limit, skip=skip)
+        if len(resources) > 0:
+            for action in resources:
+                yield action
