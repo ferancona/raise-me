ebe1f3b987fca0e6547ce5819ee4ff5ed361601b
Fernando Ancona <f.anconac@gmail.com>
2022-09-26 18:54:43 +0100

    refactor(raise-me): change resources path for packaging

--- raise_me/__init__.py
+++ raise_me/__init__.py
@@ -4,8 +4,8 @@ import pathlib
 
 
 PROJECT_PATH = pathlib.Path(__file__).resolve().parent.parent
-RESOURCES_PATH = PROJECT_PATH / 'resources'
 TESTS_PATH = PROJECT_PATH / 'tests'
+RESOURCES_PATH = PROJECT_PATH / 'raise_me' / 'resources'
 
 RAISE_DEPLOYMENT_PATH = PROJECT_PATH / 'raise-events.yaml'
 RAISE_CONFIG_PATH = PROJECT_PATH / 'raise-config.yaml'
