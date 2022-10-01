3a27c91498f92f504b4a5f579bd5bbd95707fe5e
Fernando Ancona <f.anconac@gmail.com>
2022-10-01 01:58:14 +0100

    feat(build): decompose CloudBuilder, AWSCloud & GCPCloud are instantiated when needed

--- raise_me/build/cloud/builder.py
+++ raise_me/build/cloud/builder.py
@@ -1,25 +1,26 @@
 import pathlib
-from typing import Dict, Union
+from typing import Dict, List, Union
 
 from .aws import AWSCloud
 from .gcp import GCPCloud
-from raise_me.models import Deployment
+from raise_me.models import Deployment, RaiseEvent
 from raise_me.parser import ConfigParser, DeploymentParser
 
 
 class CloudBuilder:
     def __init__(self, config_path: Union[str, pathlib.Path]) -> None:
         self.conf: Dict = ConfigParser.from_yaml(path=config_path)
-        self.aws_cloud = AWSCloud(config=self.conf)
-        self.gcp_cloud = GCPCloud(config=self.conf)
     
     def update_stack(self, events_path: Union[str, pathlib.Path]) -> None:
         deployment: Deployment = DeploymentParser.from_yaml(path=events_path)
         
-        self.aws_cloud.update_stack(
-            events=[event for event in deployment.events 
-            if event.source.provider == 'aws'])
+        aws_events: List[RaiseEvent] = [event for event in deployment.events 
+                                        if event.source.provider == 'aws']
+        gcp_events: List[RaiseEvent] = [event for event in deployment.events 
+                                        if event.source.provider == 'gcp']
+
+        if len(aws_events) > 0:
+            AWSCloud(config=self.conf).update_stack(events=aws_events)
         
-        self.gcp_cloud.update_stack(
-            events=[event for event in deployment.events 
-            if event.source.provider == 'gcp'])
+        if len(gcp_events) > 0:
+            GCPCloud(config=self.conf).update_stack(events=gcp_events)
